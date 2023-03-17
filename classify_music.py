45cf4637ebb5f8eb9aec886af9d1202c6f1af152
Joel Kärn <karn.joel@gmail.com>
2023-03-17 11:57:05 +0100

    implementation of confusion matrix instead of just accuracy

--- classify_music.py
+++ classify_music.py
@@ -1,14 +1,14 @@
 import glob
 import os
 import random
-
 import argparse
-
 import numpy as np
 from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -30,7 +30,20 @@ if __name__ == "__main__":
 
     print(X.shape, y.shape)
 
-    # Run cross-validation
+    # Run cross-validation with confusion matrix evaluation
     clf = make_pipeline(StandardScaler(), SVC())
     scores = cross_val_score(clf, X, y, cv=10)
+    y_pred = cross_val_predict(clf, X, y, cv=10)
+    conf_mat = confusion_matrix(y, y_pred)
+
     print('{:.1f} +- {:.1f}'.format(np.mean(scores) * 100, np.std(scores) * 100))
+    print(conf_mat)
+
+    # Plot confusion matrix with labels
+    labels = sorted(set(y))
+    disp = ConfusionMatrixDisplay(confusion_matrix=conf_mat,
+                                  display_labels=labels)
+    disp.plot()
+
+    # Save plot as image file
+    plt.savefig('conf_mat.png')
